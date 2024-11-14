2cb8cc42995d250e45ca66d42d8bbb165f25978e
soham-tejani <sametejani123@gmail.com>
2024-11-14 16:55:28 +0530

    Add error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,33 @@
 class ApplicationController < ActionController::API
+  # protect_from_forgery
   before_action :configure_permitted_parameters, if: :devise_controller?
+  # enable_authorization unless: :devise_controller?
+  #
+  #
+  #   rescue_from StandardError do |exception|
+  #     render json: { error: "Internal Server Error", message: exception.message, exception: exception.inspect },
+  # status: 500
+  #   end
+
+  rescue_from CanCan::AccessDenied do |exception|
+    render json: { error: "Access Denied", message: exception.message }, status: :forbidden
+  end
+
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    render json: { error: "Record not found", message: exception.message }, status: :not_found
+  end
+
+  rescue_from ActiveRecord::RecordInvalid do |exception|
+    render json: { error: "Invalid record", message: exception.message }, status: :unprocessable_entity
+  end
+
+  rescue_from ActiveRecord::RecordNotDestroyed do |exception|
+    render json: { error: "Record not destroyed", message: exception.message }, status: :unprocessable_entity
+  end
+
+  rescue_from ActionController::RoutingError do |exception|
+    render json: { error: "Routing Error", message: exception.message }, status: :not_found
+  end
 
   protected
 
